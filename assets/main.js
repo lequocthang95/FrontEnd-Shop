b360c095558f3cf7db2e323090b763bd6aafa502
waribiki95 <77254241+waribiki95@users.noreply.github.com>
2021-03-23 11:17:23 +0700

    up

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,28 @@
 // search active
 var search = document.querySelector('.search_grid');
 var searchActive = document.querySelector('.search_grid-active')
-document.querySelector('.menu_icon-search').onclick = () =>{
-    search.style.display="block";
-    searchActive.style.display="block";
-}
-
+var searchIcons = document.querySelectorAll('.menu_icon-search');
+searchIcons.forEach((searchIcon) =>{
+    searchIcon.onclick = () =>{
+        search.style.display="block";
+        searchActive.style.display="block";
+    } 
+})
 searchActive.onclick = () =>{
     search.style.display="none";
-}
+}  
+
 
 // cart active
 var cart = document.querySelector('.cart_grid');
 var cartActive = document.querySelector('.cart_grid-active')
-document.querySelector('.menu_icon-cart').onclick = () =>{
-    cart.style.display="block";
-    cartActive.style.display="block";
-}
+var cartIcons = document.querySelectorAll('.menu_icon-cart');
+cartIcons.forEach((cartIcon) =>{
+    cartIcon.onclick = () =>{
+        cart.style.display="block";
+        cartActive.style.display="block";
+    }
+})
 var cartClose = document.querySelector('.cart_close')
 cartClose.onclick = () =>{
     cart.style.display="none";
@@ -30,17 +36,17 @@ var removeBtns = document.querySelectorAll('.cart_item-remove')
 removeBtns.forEach((removeBtn) =>{
     removeBtn.onclick = () => {
         var cartItem = removeBtn.parentElement;
+        console.log(cartItem)
         var promise = Promise.resolve();
             promise
+                // .then(()=>{
+                //     return new Promise((resolve) =>{
+                //         cartInfor.style="animation: removeItem 0.3s linear";
+                //         setTimeout(resolve,300)
+                //     })
+                // })
                 .then(()=>{
                     return new Promise((resolve) =>{
-                        removeBtn.style="animation: removeItem 0.3s linear";
-                        setTimeout(resolve,300)
-                    })
-                })
-                .then(()=>{
-                    return new Promise((resolve) =>{
-                        removeBtn.style="display:none"
                         cartItem.parentElement.style="animation: removeItem 1s linear";
                         setTimeout(resolve,1000)
                     })
@@ -49,25 +55,38 @@ removeBtns.forEach((removeBtn) =>{
                     console.log(cartItem.parentElement)
                     cartItem.parentElement.style.display="none";
                 })
-        
-    }
-    
+    } 
 })
         
 
 // list active
 var list = document.querySelector('.list_grid');
 var listActive = document.querySelector('.list_grid-active')
-document.querySelector('.menu_icon-list').onclick = () =>{
-    list.style.display="block";
-    listActive.style.display="block";
-}
+var listIcons = document.querySelectorAll('.menu_icon-list');
+listIcons.forEach((listIcon) =>{
+    listIcon.onclick = () =>{
+        list.style.display="block";
+        listActive.style.display="block";
+    }
+})
 var listClose = document.querySelector('.list_close')
 listClose.onclick = () =>{
     list.style.display="none";
 }
 listActive.onclick = () =>{
     list.style.display="none";
+}
+
+// menu responsive
+var menu = document.querySelector('.respon_menu');
+document.querySelector(".menu_btn").onclick = () =>{
+    if (window.getComputedStyle(menu).display === "none") {
+        menu.style.display="block";
+    }
+    else {
+        menu.style.display="none";
+    }
+    
 }
 
 // slick product slide
@@ -83,6 +102,13 @@ $(function () {
     });
 });
 
+// header support languages
+$.fn.selectpicker.Constructor.BootstrapVersion = '4';
+$.fn.selectpicker.Constructor.DEFAULTS.multipleSeparator = ' | ';
+
+$(function(){
+    $('.my_select').selectpicker();
+});
 
 
 
