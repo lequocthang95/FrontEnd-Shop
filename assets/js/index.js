78868375950dafcc768f8dba0b2dbafe837974ad
waribiki95 <77254241+waribiki95@users.noreply.github.com>
2021-04-07 22:00:49 +0700

    up check out

---
+++ assets/js/index.js
@@ -0,0 +1,76 @@
+// slick product slide
+$(function () {
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '<button class="btn product_btn1" onclick="plusDivs(-1)" style="padding: 0;"><img class="product_icon-left" src="./assets/image_page/icon-1/01_home-left.png" alt=""></button>',
+        nextArrow: '<button class="btn product_btn2" onclick="plusDivs(1)" style="padding: 0;"><img class="product_icon-right" src="./assets/image_page/icon-1/01_home-right.png" alt=""></button>',    
+    });
+});
+var slideIndex = 1;
+showDivs(slideIndex);
+function plusDivs(n) {
+    var btnProduct1 = document.querySelector('.product_btn1');
+    var btnProduct2 = document.querySelector('.product_btn2');
+    var promise = Promise.resolve();
+    promise
+        .then(()=>{
+            slideIndex = slideIndex + n;
+            switch(slideIndex) {
+                case 3:
+                    slideIndex = 0;
+                break;
+                case -1:
+                    slideIndex = 2;
+                break;    
+            } 
+            btnProduct1.setAttribute("disabled","disabled");
+            btnProduct2.setAttribute("disabled","disabled");
+            return slideIndex;
+        })
+        .then(()=>{
+            return new Promise((resolve) =>{
+                showDivs(slideIndex);
+                setTimeout(resolve,600)
+            })
+        })
+        .then(()=>{ 
+            btnProduct1.removeAttribute("disabled","disabled");  
+            btnProduct2.removeAttribute("disabled","disabled");
+        })       
+}
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+function showDivs(n) {   
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("product_slide-item");
+  if (n > x.length) {slideIndex = 0}    
+  if (n < 0) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("orange");
+  }
+  if (n = 1){ dots[slideIndex].classList.add("orange")}
+  x[slideIndex].style.display = "flex";
+  dots[slideIndex + 3].classList.add("orange")  
+  if (dots.length > 6){
+    dots[slideIndex + 6].classList.add("orange")   
+    dots[slideIndex + 9].classList.add("orange")  
+  }
+}
+
+
+
+// header support languages
+$.fn.selectpicker.Constructor.BootstrapVersion = '4';
+$.fn.selectpicker.Constructor.DEFAULTS.multipleSeparator = ' | ';
+
+$(function(){
+    $('.my_select').selectpicker();
+});
--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,39 @@
+// slide cart
+var slideN = 0;
+showItems(slideN);
+
+function plusItems(n) {
+    slideN += n;
+    switch(slideN) {
+        case 3:
+            slideN = 0;
+        break;
+        case -1:
+            slideN = 2;
+        break;    
+    }
+  showItems(slideN);
+}
+
+function currentItem(n) {
+  showItems(slideN = n);
+}
+
+function showItems(n) {
+    var i;
+    var x = document.getElementsByClassName("cart_slide-show");
+    var dots = document.getElementsByClassName("cart_slide-item");
+    if (n > x.length) {slideN = 0}    
+    if (n < 0) {slideN = x.length}
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].classList.remove("orange");
+    }
+    if (n = 1){ dots[slideN].classList.add("orange")}
+    x[slideN].style.display = "flex";
+}
 // search active
 var search = document.querySelector('.search_grid');
 var searchActive = document.querySelector('.search_grid-active')
@@ -158,7 +194,6 @@ $.fn.selectpicker.Constructor.DEFAULTS.multipleSeparator = ' | ';
 $(function(){
     $('.my_select').selectpicker();
 });
-
 
 
 
