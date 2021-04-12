0e3b0e42265df2a24c908ff90ac098c5c8f5a4f7
waribiki95 <77254241+waribiki95@users.noreply.github.com>
2021-04-12 16:27:04 +0700

    up detaails xong

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,11 +1,11 @@
 var slideN = 0;
-showItems(slideN);
+showImgs(slideN)
 
-function currentItem(n) {
-  showItems(slideN = n);
+function currentImg(n) {
+  showImgs(slideN = n);
 }
 
-function showItems(n) {
+function showImgs(n) {
     var i;
     var x = document.getElementsByClassName("details_img-big");
     var dots = document.getElementsByClassName("details_img-small");
@@ -19,4 +19,34 @@ function showItems(n) {
     }
     if (n = 1){ dots[slideN].style.border="1px solid #2a9cf5"}
     x[slideN].style.display = "flex";
+}
+
+var amount = document.querySelector('.amount_present input');
+function amountChange(n){
+  var btnReduc = document.querySelector('.amount_reduc');
+  amount.value = parseInt(amount.value) + n;
+  if ( parseInt(amount.value) <= 0){
+      btnReduc.setAttribute("disabled","disabled")
+      amount.value= 1;
+  }
+}
+
+showItems(slideN);
+function currentItem(n) {
+  showItems(slideN = n);
+}
+function showItems(n) {
+  var i;
+  var x = document.getElementsByClassName("detail_slide-show");
+  var dots = document.getElementsByClassName("detail_slide-item");
+  if (n > x.length) {slideN = 0}    
+  if (n < 0) {slideN = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("orange");
+  }
+  if (n = 1){dots[slideN].classList.add("orange")}
+  x[slideN].style.display = "flex";
 }
